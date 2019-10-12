a7da7abed6ee96ce7a0668c715f5b107c61976f5
sjz-suyi <sjz_suyi@163.com>
2019-10-12 12:48:16 +0800

    hello world

---
+++ Helloword.java
@@ -0,0 +1,12 @@
+public class Test{
+
+public static void main(String args[]){
+
+System.out.println("Hello World.");
+System.out.println("3.");
+System.out.println("Hello World.");
+System.out.println("4");
+
+}
+
+}
